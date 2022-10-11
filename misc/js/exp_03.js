79d882eb955382fab43b37a205eecb6ab461d0e8
Mastermind2025 <85400723+Mastermind2025@users.noreply.github.com>
2022-10-11 22:17:54 +0300

    zzz

---
+++ misc/js/exp_03.js
@@ -0,0 +1,6 @@
+
+const elemList = document.querySelector('ul.list').firstElementChild.textContent;//из первого элемента текст
+const elemListEnd = document.querySelector('ul.list').lastElementChild.textContent;//из последнего элемента текст
+
+console.log(elemList);
+console.log(elemListEnd);

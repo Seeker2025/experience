44c2157a72d4b41fa833636694979301214d7260
Mastermind2025 <85400723+Mastermind2025@users.noreply.github.com>
2022-10-15 00:22:15 +0300

    zzz

--- misc/js/exp_05.js
+++ misc/js/exp_05.js
@@ -47,35 +47,115 @@
 //Задача 21/48
 
 
-const books = [
-  {
-    title: "The Last Kingdom",
-    author: "Bernard Cornwell",
-    rating: 8.38,
-  },
-  {
-    title: "Beside Still Waters",
-    author: "Robert Sheckley",
-    rating: 8.51,
-  },
-  {
-    title: "The Dream of a Ridiculous Man",
-    author: "Fyodor Dostoevsky",
-    rating: 7.75,
-  },
-  { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
-  { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
-];
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     rating: 8.51,
+//   },
+//   {
+//     title: "The Dream of a Ridiculous Man",
+//     author: "Fyodor Dostoevsky",
+//     rating: 7.75,
+//   },
+//   { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//   { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+// ];
 
-const MIN_RATING = 8;
-const AUTHOR = "Bernard Cornwell";
+// const MIN_RATING = 8;
+// const AUTHOR = "Bernard Cornwell";
 
-// метод filter возвращает новый массив с книгами, рейтинг которых больше чем 8. Деструктуризация объекта
-const topRatedBooks = books.filter(({ rating }) => rating >= MIN_RATING);
-console.log(topRatedBooks);
-// здесь новый массив с книгами Bernard Cornwell. Деструктуризация объекта
-const booksByAuthor = books.filter(({ author }) => author === AUTHOR);
-console.log(booksByAuthor);
+// // метод filter возвращает новый массив с книгами, рейтинг которых больше чем 8. Деструктуризация объекта
+// const topRatedBooks = books.filter(({ rating }) => rating >= MIN_RATING);
+// console.log(topRatedBooks);
+// // здесь новый массив с книгами Bernard Cornwell. Деструктуризация объекта
+// const booksByAuthor = books.filter(({ author }) => author === AUTHOR);
+// console.log(booksByAuthor);
+
+//======================================================================
+//Задача 22/48
+
+const users = [
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    gender: "male"
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    gender: "female"
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    gender: "male"
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    gender: "female"
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+    isActive: true,
+    balance: 3951,
+    gender: "male"
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    isActive: false,
+    balance: 1498,
+    gender: "male"
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    gender: "female"
+  }
+]
+// выбираем пользователей с цветом заданным в color. Зачем здесь map?
+const color = "brown";
+// const getUsersWithEyeColor = (users, color) => {
+// return  users.filter(user => user.eyeColor === color).map(user=>user);
+// }
+
+// Тоже самое. Кратка запись без return и фигурных скобок
+const getUsersWithEyeColor = (users, color) => users.filter(user => user.eyeColor === color).map(user=>user);
+
+
+console.log(getUsersWithEyeColor(users, color));
 
 
  
---
+++ misc/js/exp_06.js
@@ -0,0 +1,100 @@
+// const isArr = ["a", "b", "c", "d", "s",];
+// // console.log(Array.isArray(isArr));
+
+// const isArr02 = isArr.map(itm => itm);//возвращает теже элементы массива
+// console.log(isArr02);
+
+//====================================================================================
+
+// Задача 24/48
+
+const users = [
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    gender: "male",
+    age: 37
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    gender: "female",
+    age: 34
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    gender: "male",
+    age: 24
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    gender: "female",
+    age: 21
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+    isActive: true,
+    balance: 3951,
+    gender: "male",
+    age: 27
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    isActive: false,
+    balance: 1498,
+    gender: "male",
+    age: 38
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    gender: "female",
+    age: 39
+  }
+]
+
+// Задача 24/48
+// Функция возвращает массив пользователей у которых есть друг с именем в параметре friendName.
+// Массив друзей пользователя хранится в свойстве friends.
+
+// const getUsersWithFriend = (users, friendName) => {
+//   return users.filter(user => user.friends.includes(friendName));
+// }
+// console.log(getUsersWithFriend(users, "Briana Decker"));
+
+// Задача 25/48
+const  getFriends = (users) => {
+  return users
+    .flatMap((friends) => friends)
+    .filter((user, index, array) => array.indexOf(user) === index);
+}
+console.log(getFriends(users));
+
